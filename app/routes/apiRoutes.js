a96182d4a7eff5d4c4437693678cafde4b5bbc69
unknown <you@example.com>
2017-09-29 20:58:47 -0500

    fixed apiRoutes and survey

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,56 +1,47 @@
-var friends = require('../data/friends.js');
-var path = require('path');
+//two routes:
+//a GET route with the url /api/friends -- this displays a JSON of possible friends
 
-// API GET Requests - when users "visit" a page. 
-var totalDifference = 0;
+var friends = require("../data/friends");
 
 module.exports = function(app){
-  app.get('/api/friends', function(req, res){
-    res.json(friends);
-  });
 
-//API POST Request-handles when user submits a form & thus submits data to the server.
-// In each of the below cases, when a user submits form data (a JSON object)
-// ...the JSON is pushed to the appropriate Javascript array
+app.get("/api/friends", function(req, res){
+	res.json(friends);
+});
 
 
-  app.post('/api/friends', function(req, res){
+app.post("/api/friends", function(req, res){
+	//user data
+	var newUser = req.body;
+	
+	var arrayForSums = [];
+	for (var i = 0; i < friends.length; i++){
+		var comparisonArray = [];
+		for (var j = 0; j < friends[i].scores.length; j++) {
+		  comparisonArray.push(Math.abs(friends[i].scores[j] - newUser.scores[j]));
+		};
+		console.log(comparisonArray);
+		var sum = comparisonArray.reduce(function(total, amount){
+			return total + amount;
+		});
+		console.log("the sum is " + sum);
+		arrayForSums.push(sum);
+		console.log("array for sums " + arrayForSums)
 
-    var greatMatch = {
-      name: "",
-      image: "",
-      matchDifference: 1000
-    };
-    var usrData   = req.body;
-    var usrName   = usrData.name;
-    var usrImage  = usrData.image;
-    var usrScores   = usrData.scores;
+	};	
+	Array.min = function( array ){
+    	return Math.min.apply( Math, array );
+};
+	var minimum = Array.min(arrayForSums);
+	console.log("min: "+ minimum);
+	var indexOfMatch = arrayForSums.indexOf(minimum);
 
-    var totalDifference = 0;
+	console.log(friends[indexOfMatch]);
+	var matchName = friends[indexOfMatch].name;
+	var match = friends[indexOfMatch];
+	friends.push(newUser);
+	res.send(match);
+});
 
-    //loop through the friends data array of objects to get each friends scores
-    for(var i = 0; i < [friends].length-1; i++){
-      console.log(friends[i].name);
-      totalDifference = 0;
 
-      //loop through that friends score and the users score and calculate the 
-      // absolute difference between the two and push that to the total difference variable set above
-      for(var j = 0; j < 10; j++){
-        // We calculate the difference between the scores and sum them into the totalDifference
-        totalDifference += Math.abs(parseInt(usrScores[j]) - parseInt(friends[i].scores[j]));
-        // If the sum of differences is less then the differences of the current "best match"
-        if (totalDifference <= greatMatch.friendDifference){
-
-          // Reset the bestMatch to be the new friend. 
-          greatMatch.name = friends[i].name;
-          greatMatch.photo = friends[i].photo;
-          greatMatch.matchDifference = totalDifference;
-        }
-      }
-    }
-
-    friends.push(usrData);
- 
-    res.json(greatMatch);
-  });
 };
